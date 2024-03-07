7ce550a76393bae28c7dff93b055d41c356e9656
Somgester <dhruvratmele13@gmail.com>
2024-03-07 22:16:20 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-// import Navbar from './components/navbar';
+import Navbar from './components/navbar';
 // import BookingForm from './components/bookingForm';
-import LoginPage from './components/loginPage';
+// import LoginPage from './components/loginPage';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar />
     <BookingForm /> */}
-    <LoginPage />
+    {/* <LoginPage /> */}
+    <Navbar/>
     </div>
   );
 }
